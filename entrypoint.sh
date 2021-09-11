1007366bdd252443c97e1bdf3ddcdf7a0426092d
Georgi Todorov <51453974+GVLTodorov@users.noreply.github.com>
2021-09-12 01:10:57 +0300

    Beta (#3)
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Entrypoint script ..."
+
 if [ -z ${TOKEN+x} ]; then
   echo "TOKEN environment variable is not set"
   exit 1
@@ -10,13 +12,37 @@ if [ -z ${REPOSITORY+x} ]; then
   exit 1
 fi
 
+echo "Preparing ..."
+
+API_VERSION=v3
+API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+AUTH_HEADER="Authorization: Bearer ${TOKEN}"
+
+API=https://api.github.com/repos
+AOO="$(echo "${REPOSITORY}" | cut -d/ -f4)"
+RNAME="$(echo "${REPOSITORY}" | cut -d/ -f5)"
+
+URL="${API}/${AOO}/${RNAME}/actions/runners/registration-token"
+
+echo "Using ${URL} ..."
+echo "Obtain Token..."
+
+RTOKEN="$(curl -XPOST -fsSL \
+  -H "${AUTH_HEADER}" \
+  -H "${API_HEADER}" \
+  "${URL}" \
+| jq -r '.token')"
+
+echo "Successful ..."
+
+echo "Configuring ..."
 cd /home/docker/actions-runner
 
-./config.sh --url $REPOSITORY --token $TOKEN --name $NAME --labels $LABEL --work ${DIR}
+./config.sh --url "${REPOSITORY}" --token "${RTOKEN}" --name "${NAME}" --labels "${LABEL}" --work "${DIR}"
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${TOKEN}
+    ./config.sh remove --unattended --token $RTOKEN
 }
 
 trap 'cleanup; exit 130' INT
